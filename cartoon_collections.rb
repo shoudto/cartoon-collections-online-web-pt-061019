50fd0313050ed7f55d3d7d59fdf728b425c56323
Oscar Shoudt <shoudto@gmail.com>
2019-09-11 16:03:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,12 +1,12 @@
 
 require 'pry'
 
-def roll_call_dwarves(dwarves) 
+def roll_call_dwarves(dwarves)
 dwarves.each_with_index do |dwarve, index|
-  num = index + 1 
+  num = index + 1
   # index = 1, ask a question about this!
-  puts "#{num}:#{dwarve}" 
-end 
+  puts "#{num}:#{dwarve}"
+end
 end
 
 def summon_captain_planet(calls)
@@ -23,5 +23,5 @@ def find_the_cheese(contains_cheddar)
 cheese_types = ["cheddar", "gouda", "camembert"]
 contains_cheddar.find do |cheese|
   cheese_types.include?(cheese)
-end 
+end
 end
